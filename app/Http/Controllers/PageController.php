d0580c02dcad5d6927705909aa4c076456540220
Sybren Dijkstra <nerbiz@gmail.com>
2025-02-05 11:39:36 +0100

    Added model code for insert and read

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Player;
+use App\Models\Team;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -16,11 +18,42 @@ class PageController extends Controller
     {
         // Maak hier teams en spelers aan, met behulp van models
         // Hier is dus geen view nodig en ook geen return
+
+        $team1 = Team::create([
+            'name' => 'BV Lwd',
+            'sport' => 'Badminton',
+        ]);
+
+        $team2 = Team::create([
+            'name' => 'Bootjesclub',
+            'sport' => 'Roeien',
+        ]);
+
+        Player::create([
+            'team_id' => $team1->id,
+            'first_name' => 'A',
+            'surname' => 'van der B',
+        ]);
+
+        Player::create([
+            'team_id' => $team2->id,
+            'first_name' => 'Henk',
+            'surname' => 'B',
+        ]);
     }
 
     public function showData(): View
     {
         // Haal alle teams en spelers op en geef het aan de 'show' view
+
+        // Met all() haal je alle rijen op uit de tabel
+        $allPlayers = Player::all();
+
+        // In plaats van alle rijen ophalen, kun je ook filteren met where()
+        $allTeams = Team::where('id', '>', 2)->get();
+
+        // dd() is de 'var_dump' van Laravel, het output een variabele en stopt dan met een exit
+        dd($allPlayers, $allTeams);
 
         return view('show');
     }
